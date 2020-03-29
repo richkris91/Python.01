f13c75eefc3aa61081bcbe421062f1ccbe30112b
kris <krzy_gorny@wp.pl>
2020-03-29 18:50:12 +0200

    Standard Update 21.02.2020

--- Homeworks/day.04/day.04 homework - 04.py
+++ Homeworks/day.04/day.04 homework - 04.py
@@ -1,46 +1,22 @@
-from Modules_01.multitool_functions import celsius_into_fahrenheit, fahrenheit_into_celsius, validator, square_maker
+from Modules_01.multitool_functions import celsius_into_fahrenheit, fahrenheit_into_celsius, validator, square_maker, egyptian_special, entry_counter
 functions = {
-    '1': 'celsius into fahrenheit', '2': 'fahrenheit into celsius',
-    '3': 'square maker', '4': 'egyptian special'
+    '1': ['Celsius into Fahrenheit', celsius_into_fahrenheit],
+    '2': ['Fahrenheit into celsius', fahrenheit_into_celsius],
+    '3': ['Square maker', square_maker],
+    '4': ['Egyptian special', egyptian_special]
 }
 
 
 def menu(function_list):
-    print('''Welcome to '0.1 Royal Multitool',
+    print('''Welcome to '0.2 Royal Multitool',
 to access one of the given functions enter a representative number''')
     for key in function_list:
-        print(key + ': ' + function_list[key])
+        print(key + ': ' + str(function_list[key][0]))
+    print('PS, '
+          'You have opened this program ' + entry_counter() + ' times')
 
 
 menu(functions)
 input_1 = (input(': '))
-int(functions[input_1])
-
-
-# if validator(input_1) and 0 < int(input_1) <= 4:
-#    number = int(input_1)
-#   if number == 1:
-#        print('You have chosen the Square maker ')
-#        input_square = input('Enter square height/length: ')
-#        if validator(input_square):
-#            square_maker(input_square)
-#    elif number == 2:
-#        print('You have chosen the Egyptian special')
-#        input_Egyptian = input('Enter pyramid height: ')
-#        if validator(input_Egyptian):
-#         x_2 = int(input_Egyptian)
-#            counter_2 = 0
-#            while counter_2 < x_2:
-#                print(' ' * 14 + ' ' * (x_2 - 1 - counter_2) + (counter_2 + 1) * '[]')
-#                counter_2 += 1
-#            print('End')
-#    elif number == 3:
-#        print('Coming soon')
-#    elif number == 4:
-#        input_4 = input('Enter degrees in Celsius: ')
-#        if validator(input_4):
-#           celsius_into_fahrenheit(input_4)
-#    elif number == 5:
-#        pass
-# else:
-#   print('You have not entered a correct number')
+if validator(input_1):
+    functions[str(input_1)][1]()
--- Modules/Modules_01/multitool_functions.py
+++ Modules/Modules_01/multitool_functions.py
@@ -1,52 +1,3 @@
-def celsius_into_fahrenheit(c):
-    f = (float(c)) * 9 / 5 + 32
-    print('''The formula for converting Celsius into Fahrenheit is
-    x= x * 9 / 5 +32
-    when x represents Celsius
-    so''')
-    print(str(c) + '*9/5+32=' + str(f))
-    print(str(c) + ':_Celsius equals ' + str(f) + ':_Fahrenheit')
-    print('End')
-
-
-def fahrenheit_into_celsius(f):
-    c = (float(f) - 32) / 1.8
-    print('''The formula for converting Fahrenheit into Celsius is
-    x = (x-32)/1.8
-    when x represents Fahrenheit
-    so''')
-    print(str(f) + '-32)/1.8 = ' + str(c))
-    print(str(f) + ':Fahrenheit equals ' + str(c) + ':Celsius')
-
-
-def entry_counter():
-    with open('counter_txt', "r+") as test:
-        x = test.read()
-        test.seek(0)
-        if x == '':
-            x = 0
-        else:
-            x = int(test.read())
-            x += 1
-        test.seek(0)
-        test.write(str(x))
-        test.seek(0)
-        return test.read()
-
-
-def square_maker(input_square):
-    x_1 = int(input_square)
-    if x_1 == 1:
-        print('+')
-        print('End')
-    else:
-        print('+' + '--' * (x_1 - 2) + '+')
-        for i in range((x_1 - 2)):
-            print('|' + '  ' * (x_1 - 2) + '|')
-        print('+' + '--' * (x_1 - 2) + '+')
-        print('End')
-
-
 def validator(maybe_a_number):
     try:
         float(maybe_a_number)/2
@@ -60,4 +11,75 @@ def validator(maybe_a_number):
     else:
         print('You have maneged to properly enter the input')
         return True
+
+
+def celsius_into_fahrenheit():
+    c = input('Enter degrees in celsius: ')
+    if validator(input):
+        f = (float(c)) * 9 / 5 + 32
+        print('''The formula for converting Celsius into Fahrenheit is
+        x = x * 9 / 5 + 32
+        when x represents Celsius
+        so''')
+        print(str(c) + '*9/5+32=' + str(f))
+        print(str(c) + ':_Celsius equals ' + str(f) + ': Fahrenheit')
+        print('End')
+
+
+def fahrenheit_into_celsius():
+    f = input('Enter degrees in fahrenheit')
+    if validator(input):
+        c = (float(f) - 32) / 1.8
+        print('''The formula for converting Fahrenheit into Celsius is
+        x = (x-32)/1.8
+        when x represents Fahrenheit
+        so''')
+        print(str(f) + '-32)/1.8 = ' + str(c))
+        print(str(f) + ':Fahrenheit equals ' + str(c) + ':Celsius')
+
+
+def entry_counter():
+    try:
+        with open('counter_txt', "r+") as test_1:
+            x = test_1.read()
+            test_1.seek(0)
+            if x == '':
+                x = 0
+            else:
+                x = int(test_1.read())
+                x += 1
+            test_1.seek(0)
+            test_1.write(str(x))
+            test_1.seek(0)
+            return test_1.read()
+    except:
+        with open('counter_txt', "w") as test_2:
+            test_2.write(str(0))
+            test_2.seek(0)
+            return test_2.read()
+
+
+def square_maker():
+    input_square = input('Enter square height/length ')
+    x_1 = int(input_square)
+    if x_1 == 1:
+        print('+')
+        print('End')
+    else:
+        print('+' + '--' * (x_1 - 2) + '+')
+        for i in range((x_1 - 2)):
+            print('|' + '  ' * (x_1 - 2) + '|')
+        print('+' + '--' * (x_1 - 2) + '+')
+        print('End')
+
+
+def egyptian_special():
+    input_egyptian = input('Enter pyramid height: ')
+    if validator(input_egyptian):
+        x_2 = int(input_egyptian)
+        counter_2 = 0
+        while counter_2 < x_2:
+            print(' ' * 14 + ' ' * (x_2 - 1 - counter_2) + (counter_2 + 1) * '[]')
+            counter_2 += 1
+        print('End')
 
