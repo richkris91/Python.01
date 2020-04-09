21f7c44905fb205e1958aeee273989e8250f8011
kris <krzy_gorny@wp.pl>
2020-04-09 19:31:02 +0200

    - Started new terminal game project:     - Text game struggle day 3

--- Projects/War Game.py
+++ Projects/War Game.py
@@ -64,6 +64,7 @@ def adding_empty_tiles():
             if Tiles[key].count(1) == 0:
                 tile_add = Tile(key, 1)
                 Tiles[key].append(tile_add)
+        Tiles[key].sort()
 
 
 def making_the_tiles_1_time():
@@ -75,16 +76,172 @@ def making_the_tiles_1_time():
             counter_making1 += 1
 
 
-def unit_create(player, units_list, sup_class, y, x):
+def unit_creating_menu(player_counter):
+    print('Select an unit witch you want to place.')
+    units_list = {1: ['Goblin', Goblin, ]
+                  }
+    for key in units_list:
+        print(str(key) + ': ' + units_list[key][0])
+    unit_type_input = int(input('Witch unit would you like to recruit?'))
+    if player_counter == 1:
+        print('Remember you can only place units on y 0 or 1 lines')
+        y = int(input('Select y line: '))
+        x = int(input('Select  x line'))
+        unit_create(int(player_counter), Units_1, units_list[unit_type_input][1], y, x)
+    elif player_counter == 2:
+        print('Remember you can only place units on y 8 or 9 lines')
+        y = int(input('Select y line: '))
+        x = int(input('Select  x line'))
+        unit_create(int(player_counter), Units_2, units_list[unit_type_input][1], y, x)
+
+
+def unit_create(player, player_units_list, sup_class, y, x):
+    print(str(Tiles[y][x]))
     if Tiles[y][x].content is None:
         del Tiles[y][x]
         number = 0
-        for element in units_list:
+        for element in player_units_list:
             number += 1
-        new_unit = sup_class(y, x, player, number)
+        new_unit = sup_class(y, x, int(number))
         Tiles[y].insert(x, new_unit)
+        setattr(Tiles[y][x], 'nr', int(number))
+        setattr(Tiles[y][x], 'player', int(player))
+        Units_1.append(str(str(sup_class) + str(number)))
     else:
         print("You can't place units on each other")
+    map_making()
+
+
+def turn_menu():
+    turn_counter = 1
+    player_counter = 1
+    command = None
+    while command != 0:
+        print('=====================================================')
+        print('Its player_ ' + str(player_counter) + ' turn = ' + str(turn_counter))
+        print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
+        map_making()
+        print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
+        command = None
+        while command != 0 and command != 9:
+            choices_list = {1: [': Recruit an unit = 1', unit_creating_menu],
+                            2: [': Move an unit = 2', moving_an_unit],
+                            3: [': Attack with an unit = 3', ],
+                            4: [': End turn = 9', ],
+                            5: [': End Game = 0', ]
+                            }
+            for key in choices_list:
+                print(str(key) + choices_list[key][0])
+            command = int(input('What do you want to do?: '))
+            print('=====================================================')
+            if command != 9 and command != 0:
+                if command == 1 or command == 2:
+                    choices_list[command][1](player_counter)
+                else:
+                    choices_list[command][1]()
+
+            elif command == 9:
+                turn_counter += 1
+                if player_counter == 1:
+                    player_counter += 1
+                else:
+                    player_counter -= 1
+            else:
+                pass
+
+
+def moving_an_unit(player_counter):
+    y = int(input('Select y line unit: '))
+    x = int(input('Select  x line unit'))
+    if Tiles[y][x].content is None:
+        print('Its not an unit')
+    unit_belonging = getattr(Tiles[y][x], 'player')
+    if str(unit_belonging) == str(player_counter):
+        unit_movement = int(getattr(Tiles[y][x], 'movement'))
+        if unit_movement == 0:
+            print('This unit is out of moves!')
+        move_dir = input("How would you like to move?"
+                         "Vertically or Horizontally? v or h:  ")
+        if move_dir == 'v':
+            up_down = input('Up or down, u or d: ')
+            how_much = int(input('By how many tiles?: '))
+            movement_counter = 0
+            if up_down == 'u':
+                while unit_movement != 0:
+                    if movement_counter <= how_much:
+                        if Tiles[y + 1][x].content is None:
+                            del Tiles[y + 1][x]
+                            obj_class = Tiles[y][x].__class__.__name__
+                            new_movement = int(getattr(Tiles[y][x], 'movement')) - 1
+                            nr = getattr(Tiles[y][x], 'nr')
+                            hp = getattr(Tiles[y][x], 'hp')
+                            del Tiles[y][x]
+                            add = obj_class(y, x, nr, new_movement, hp)
+                            Tiles[y + 1][x].insert(add)
+                            y += 1
+                    else:
+                        adding_empty_tiles()
+            elif up_down == 'd':
+                while unit_movement != 0:
+                    if movement_counter <= how_much:
+                        if Tiles[y - 1][x].content is None:
+                            obj_class = Tiles[y][x].__class__.__name__
+                            new_movement = int(getattr(Tiles[y][x], 'movement')) - 1
+                            nr = getattr(Tiles[y][x], 'nr')
+                            hp = getattr(Tiles[y][x], 'hp')
+                            del Tiles[y][x]
+                            del Tiles[y - 1][x]
+                            add = obj_class(y, x, nr, new_movement, hp)
+                            Tiles[y - 1][x].insert(add)
+                            y -= 1
+                    else:
+                        adding_empty_tiles()
+        elif move_dir == 'h':
+            left_right = input('Left or right - l or r:  ')
+            how_much = int(input('By how many tiles? '))
+            movement_counter = 0
+            if left_right == 'l':
+                while unit_movement != 0:
+                    if movement_counter <= how_much:
+                        if Tiles[y][x - 1].content is None:
+                            del Tiles[y][x - 1]
+                            obj_class = Tiles[y][x].__class__.__name__
+                            new_movement = int(getattr(Tiles[y][x], 'movement')) - 1
+                            nr = getattr(Tiles[y][x], 'nr')
+                            hp = getattr(Tiles[y][x], 'hp')
+                            del Tiles[y][x]
+                            add = obj_class(y, x, nr, new_movement, hp)
+                            Tiles[y][x - 1].insert(add)
+                            x -= 1
+                    else:
+                        adding_empty_tiles()
+
+            if left_right == 'r':
+                while unit_movement != 0:
+                    if movement_counter <= how_much:
+                        if Tiles[y][x + 1].content is None:
+                            del Tiles[y][x + 1]
+                            obj_class = Tiles[y][x].__class__.__name__
+                            new_movement = int(getattr(Tiles[y][x], 'movement')) - 1
+                            nr = getattr(Tiles[y][x], 'nr')
+                            hp = getattr(Tiles[y][x], 'hp')
+                            del Tiles[y][x]
+                            add = obj_class(y, x, nr, new_movement, hp)
+                            Tiles[y][x + 1].insert(add)
+                            x += 1
+                    else:
+                        adding_empty_tiles()
+    else:
+        print('This unit does not follow four commands!')
+
+
+def war_game():
+    Gold_1 = 10
+    Gold_2 = 10
+    making_the_tiles_1_time()
+    game = 0
+    turn_menu()
+    print('You have left the game')
 
 
 class Tile(object):
@@ -101,11 +258,10 @@ class Tile(object):
 
 
 class Unit(Tile):
-    def __init__(self, y, x, hp, nr, movement, player, content=None):
+    def __init__(self, y, x, hp, nr, movement, content=None):
         super().__init__(y, x, content)
         self.hp = hp
         self.movement = movement
-        self.player = player
         self.nr = nr
         if hp == 0:
             del self
@@ -114,78 +270,14 @@ class Unit(Tile):
         text = self.print
         return text
 
-    def move_unit(self, y, x):
-        if Tiles[y][x].content is None:
-            print('Its not an unit')
-        if self.movement == 0:
-            print("This unit does not have any leftover movement points")
-        move_dir = input('How would you like to move?'
-                         'Vertically or Horizontally? v or h:  ')
-        if move_dir == 'v':
-            up_down = input('Up or down, u or d: ')
-            how_much = int(input('By how many tiles? '))
-            movement_counter = 0
-            if up_down == 'u':
-                while self.movement != 0:
-                    if movement_counter <= how_much:
-                        if Tiles[self.y + 1][x].content is None:
-                            del Tiles[self.y + 1][x]
-                            unit_create(self.player, Units_1, self.__class__, self.y + 1, self.x)
-                            Tiles[self.y + 1][x].movement -= 1
-                            del self
-                            movement_counter += 1
-                    else:
-                        adding_empty_tiles()
-            elif up_down == 'd':
-                while self.movement != 0:
-                    if movement_counter <= how_much:
-                        if Tiles[self.y - 1][x].content is None:
-                            del Tiles[self.y - 1][x]
-                            unit_create(self.player, Units_1, self.__class__, self.y - 1, self.x)
-                            Tiles[self.y - 1][x].movement -= 1
-                            del self
-                            movement_counter += 1
-                    else:
-                        adding_empty_tiles()
-        elif move_dir == 'h':
-            left_right = input('Left or right - l or r:  ')
-            how_much = int(input('By how many tiles? '))
-            movement_counter = 0
-            if left_right == 'l':
-                while self.movement != 0:
-                    if movement_counter <= how_much:
-                        if Tiles[self.y][x - 1].content is None:
-                            del Tiles[self.y][x - 1]
-                            unit_create(self.player, Units_1, self.__class__, self.y, self.x - 1)
-                            Tiles[self.y][x - 1].movement -= 1
-                            del self
-                            movement_counter += 1
-                    else:
-                        adding_empty_tiles()
-            if left_right == 'r':
-                while self.movement != 0:
-                    if movement_counter <= how_much:
-                        if Tiles[self.y][x + 1].content is None:
-                            del Tiles[self.y][x + 1]
-                            unit_create(self.player, Units_1, self.__class__, self.y, self.x + 1)
-                            Tiles[self.y][x + 1].movement -= 1
-                            del self
-                            movement_counter += 1
-                    else:
-                        adding_empty_tiles()
-
 
 class Goblin(Unit):
-    def __init__(self, y, x, player, nr, movement=5, taken_dmg=0, hp=10, content='Gob'):
-        super().__init__(y, x, hp, nr, movement, player, content)
+    def __init__(self, y, x, nr=0, player=None, movement=5, taken_dmg=0, hp=10, content='Gob'):
+        super().__init__(y, x, hp, nr, movement)
         self.content = content
         self.hp = hp - taken_dmg
         self.nr = nr
         self.player = player
 
 
-making_the_tiles_1_time()
-# Actual Game
-unit_create(1, Units_1, Goblin, 7, 6)
-map_making()
-Tiles[7][6].move_unit(7, 6)
+war_game()
