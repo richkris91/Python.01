45f8e4215c1e3abdd713bc920141065bffbb021c
kris <krzy_gorny@wp.pl>
2020-05-09 15:10:09 +0200

    - Continued making log text reader #5

--- Projects/file_reader.py
+++ Projects/file_reader.py
@@ -1,5 +1,4 @@
 import re
-import xlrd
 import xlwt
 
 library = []
@@ -10,9 +9,9 @@ class file(object):
         self.lines = lines
         self.nr = nr
         self.time = time
-        self.port = port
-        self.Host_r = re.split(' ', self.port)[0]
-        self.If_name = re.split(' ', self.port)[1]
+        self.port = re.split(' ', port)
+        self.Host_r = self.port[1]
+        self.If_name = self.port[2]
         self.description = description
         self.RE = RE.strip(',')
         self.RE_min = re.split('/', self.RE)[0].strip(',')
@@ -29,39 +28,40 @@ class file(object):
         self.RX_p = RX_p
 
     def show(self):
-        return str(self.nr) + '\n' + str(len(self.lines)) + '\n' + str(self.time) + '\n' + str(self.por) + '\n' + str(
+        return str(self.nr) + '\n' + str(len(self.lines)) + '\n' + str(self.time) + '\n' + str(self.port) + '\n' + str(
             self.description)
 
     def data_table(self):
-        return_ls = []
-        return_ls.append(str(self.nr))
-        return_ls.append(str(self.Host_r))
-        return_ls.append(str(self.time))
-        return_ls.append(str(self.If_name))
-        return_ls.append(str(self.description))
-        return_ls.append(str(self.RE))
-        return_ls.append(str(int(self.RE_min) * 100 / int(self.RE_max)))
-        return_ls.append(str(self.TX))
-        return_ls.append(str(self.TX_b))
-        return_ls.append(str(int(self.TX_b) / 1000))
-        return_ls.append(str(self.TX_p))
-        return_ls.append(str(int(self.TX_min) * 100 / int(self.TX_max)))
-        return_ls.append(str(self.RX))
-        return_ls.append(str(self.RX_b))
-        return_ls.append(str(int(self.RX_b) / 1000))
-        return_ls.append(str(self.RX_p))
-        return_ls.append(str(int(self.RX_min) * 100 / int(self.RX_max)))
+        return_ls = [str(self.nr), str(self.Host_r), str(self.time), str(self.If_name), str(self.description),
+                     str(self.RE), str(round(int(self.RE_min) * 100 / int(self.RE_max), 2)),
+                     str(self.TX), str(round(int(self.TX_min) * 100 / int(self.TX_max), 2)), str(self.TX_b),
+                     str(int(self.TX_b) / 1000), str(self.TX_p),
+                     str(self.RX), str(round(int(self.RX_min) * 100 / int(self.RX_max), 2)), str(self.RX_b),
+                     str(int(self.RX_b) / 1000), str(self.RX_p)]
         return return_ls
 
 
-def show_data_table():
-    try:
-        workbook = xlrd.open_workbook('cisc.xlsx')
-        worksheet = workbook.sheet_by_index(0)
-    except FileNotFoundError:
-        wb = xlwt.Workbook()
-        newsheet = wb.add_sheet('cisc.xlsx')
-        newsheet.write(1, 1, 'contents here')
+def table_data():
+    wb = xlwt.Workbook()
+    new_sheet = wb.add_sheet('cisc.xlsx')
+    data = [
+        'Object Nr', 'Host attribute', 'Date', 'If_name', 'Description',
+        'RE', 'RE_%',
+        'TX', 'TX_%', 'TX_b/s', 'TX_Mb/s', 'TX_packets',
+        'RX', 'RX_%', 'RX_b/s', 'RX_MB/s', 'TX_packets'
+    ]
+    counter = 0
+    for element in data:
+        new_sheet.write(0, counter, element)
+        counter += 1
+    counter2 = 1
+    for element in range(len(library)):
+        counter3 = 0
+        while counter3 <= 16:
+            new_sheet.write(counter2, counter3, library[element].data_table()[counter3])
+            counter3 += 1
+        counter2 += 1
+    wb.save('cisc.xlsx')
 
 
 def file_reader():
@@ -80,28 +80,31 @@ def file_reader():
                       }
         lines_f = []
         description_f = ''
+        time_f = ''
+        port_f = ''
+        RE = ''
+        TX = ''
+        RX = ''
+        TX_l = ''
+        TX_b = ''
+        TX_p = ''
+        RX_l = ''
+        RX_b = ''
+        RX_p = ''
         for element in files_0:
-            time_f = ''
-            port_f = ''
-            description_f = ''
-            RE = ''
-            TX = ''
-            RX = ''
-            TX_b = ''
-            TX_p = ''
-            RX_b = ''
-            RX_p = ''
             if 'date and time' in element:
                 time_f = element[16:]
                 time_f = time_f.strip('\n')
             if 'Port pl' in element:
-                port_f = element.strip('Port ')
-                port_f = port_f.strip('\n')
-                port_f = port_f.strip(':')
+                port_f = element.strip('\n')
+                port_f = port_f.strip('Port')
+                port_f = port_f.replace(' :', '')
             if 'Description' in element:
-                description_f = element.strip('Description: ')
+                description_f = element.replace('Description: ', '')
+                description_f = description_f.replace('\n', '')
+                description_f[0].replace(' ', '')
             if 'reliability' and 'txload' and 'rxload' in element:
-                RE_ls = re.split(' ', element)
+                RE_ls = re.split(' ', element.strip())
                 RE = RE_ls[1]
                 TX = RE_ls[3]
                 RX = RE_ls[5]
@@ -110,7 +113,14 @@ def file_reader():
                 TX_b = TX_l[2]
                 TX_p = TX_l[4]
             if 'second input rate' in element:
-                RX_l = re.split(' ', element)
+                RX_f = element.replace('30 second input rate', '')
+                RX_f1 = RX_f.replace('bits/sec,', '')
+                RX_f2 = RX_f1.replace(' packets/sec\n', '')
+                for element in range(len(RX_f2)):
+                    if RX_f2 == '':
+                        del.RX_f2
+                RX_l = re.split(' ', RX_f2)
+                print(RX_l)
                 RX_b = RX_l[2]
                 RX_p = RX_l[4]
             if element == '\n':
@@ -128,5 +138,4 @@ def file_reader():
 
 
 file_reader()
-for element in library:
-    print(element.show())
+table_data()
