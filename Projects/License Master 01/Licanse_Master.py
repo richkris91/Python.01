b9871f7aeb5d6362d56314030f05e2c3f5f67d52
kris <krzy_gorny@wp.pl>
2020-05-20 14:49:57 +0200

    - Log Master 03# Operational 01

--- Projects/License Master 01/Licanse_Master.py
+++ Projects/License Master 01/Licanse_Master.py
@@ -3,75 +3,15 @@
 import os
 
 
-# files = os.listdir('week_20')
-
-def license_master():
-    menu()
-
-
-def menu():
-    go = 1
-    while go == 1:
-        commands = {
-            0: ['Exit'],
-            1: ['User', user_logs],
-            2: ['Logs uses'],
-            3: ['Computers ']
-        }
-        print('-' * 40)
-        for key in commands:
-            print(str(key) + ': ' + commands[key][0])
-        command = input('Enter what would you like to do? :')
-        print('-' * 40)
-        try:
-            r_com = int(command)
-            if r_com > len(commands):
-                print('You have not managed to enter the input correctly')
-            else:
-                if r_com == 0:
-                    go -= 1
-                else:
-                    commands[r_com][1]()
-        except:
-            print('You have not managed to enter the input correctly')
-
-
-def is_in_dic(dic, obj):
-    for key in dic:
-        if str(obj) in key:
-            return True
-    return False
-
-
-def user_logs():
-    # opening files
-    users = {
-        'Users': '\n'
-    }
-    log_fracts = []
-    files = os.listdir('week_20')
-    # extracting data
-    for element in files:
-        element1 = element.replace("'", '')
-        with open('./week_20/' + element1, 'r+') as opened_file:
-            content = opened_file.read().split('\n')
-            for element in range(len(content)):
-                if ' IN: ' in content[element] or ' OUT: ' in content[element]:
-                    if ' ids_l ic@pl01-lic5' not in content[element]:
-                        log_fract = content[element].split(' ')
-                        log_fracts.append(log_fract)
-    print('-' * 20)
-    user_menu = {
-        'enter': [': Show all info'],
-        'x    ': [': Exit']
-    }
-    for key in user_menu:
-        print(key + user_menu[key][0])
-    print('-' * 20)
-    input_user = input('What would you like to do?')
-    if input_user == '':
-        # Creating user objects
-        for element in log_fracts:
+def user_creator(users, log_fracts):
+    files_name = 'None'
+    users = users
+    log_fracts = log_fracts
+    for element in log_fracts:
+        if '__FILE' in element:
+            files_name = str(element).split(':')[1]
+            print(files_name)
+        else:
             lic_count = ''
             if len(element) >= 7 and '(' in str(element[:][6:7]):
                 lic_count = element[:][6:7][0]
@@ -84,72 +24,154 @@ def user_logs():
                 users[username] = new_user
             if is_in_dic(users, username):
                 if lic_count != '':
-                    new_log = log(element[0], element[2], element[3], username, computer, lic_count)
+                    new_log = log(element[0], element[2], element[3], username, computer, files_name, lic_count)
                 else:
-                    new_log = log(element[0], element[2], element[3], username, computer)
+                    new_log = log(element[0], element[2], element[3], username, computer, files_name)
                 users[username].add_req(new_log)
                 comps = users[username].show_comps()
                 if str(computer) not in comps:
                     users[username].add_comp(computer)
-        del users['Users']
-        counter_name_list = 6
-        users_list = []
-        for key in users:
-            if counter_name_list == 6:
-                users_list.append([])
-                counter_name_list = 0
-            else:
-                users_list[len(users_list) - 1].append(': ' + key + ' ' * (8 - len(key)))
-                counter_name_list += 1
-        for element in range(len(users_list)):
-            if len(users_list[element]) != 0:
-                print(str(users_list[element][:]))
-        input_all = input('=' * 84 + '\nTo witch user would you like to apply this command?\n'
-                                     'Press enter to apply to every one of the above:\n')
-        if input_all == '':
-            counter = 1
-            for key in users:
-                print(str(counter) + ': User : ' + users[key].show_user()[0])
-                print(' ' * 5 + 'Number of Computers: ' + str(len(users[key].show_user()[1])))
-                print(' ' * 5 + 'Number of logs: ' + str(len(users[key].show_user()[2])))
-                print(' ' * 8 + 'Computers')
-                comp_count = 0
-                comps_logs = users[key].comps_logs()
-                for key in comps_logs:
-                    print(' ' * 10 + 'Computer: ' + key)
-                    sorted_logs = log_sorter(comps_logs[key])
-                    for key in sorted_logs:
-                        for element in sorted_logs[key]:
-                            print(' ' * 12 + element.show_log())
-                        print('^' * 84)
+    return users
 
-                comp_count += 1
-                counter += 1
-        else:
+
+def is_in_dic(dic, obj):
+    for key in dic:
+        if str(obj) in key:
+            return True
+    return False
+
+
+def log_master():
+    go = 1
+    users_count = 1
+    # opening files
+    users = {
+        'Users': '\n'
+    }
+    log_fracts = []
+    files = os.listdir('week_20')
+    # extracting data
+    for element in files:
+        element1 = element.replace("'", '')
+        log_fracts.append('____FILE:' + str(element.split('_', 2)[1] + '_' + str(element.split('_', 2)[2])))
+        with open('./week_20/' + element1, 'r+') as opened_file:
+            content = opened_file.read().split('\n')
+            for element in range(len(content)):
+                if ' IN: ' in content[element] or ' OUT: ' in content[element]:
+                    if ' ids_l ic@pl01-lic5' not in content[element]:
+                        log_fract = content[element].split(' ')
+                        log_fracts.append(log_fract)
+    while go == 1:
+        print('-' * 20)
+        user_menu = {
+            'enter': ': Show all info',
+            'c    ': ': create text file',
+            'x    ': ': Exit'
+        }
+        for key in user_menu:
+            print(key + user_menu[key])
+        print('-' * 20)
+        input_user = input('What would you like to do?')
+        # Don't mind this
+        while users_count != 0:
+            del users['Users']
+            users_count -= 1
+        #
+        if input_user == 'x':
+            go -= 1
+        elif input_user == '':
+            # Creating user objects
+            users = user_creator(users, log_fracts)
+            #
+            counter_name_list = 6
+            users_list = []
+            # user list print
             for key in users:
-                if input_all == key:
-                    user_ = input_all
-                    print('User : ' + users[user_].show_user()[0])
-                    print(' ' * 5 + 'Number of Computers: ' + str(len(users[user_].show_user()[1])))
-                    print(' ' * 5 + 'Number of logs: ' + str(len(users[user_].show_user()[2])))
-                    print(' ' * 8 + 'Computers')
-                    comps_logs = users[user_].comps_logs()
-                    for key in comps_logs:
-                        print(' ' * 10 + key)
-                        for element in comps_logs[key]:
-                            print(' ' * 12 + element.show_log())
+                if counter_name_list == 6:
+                    users_list.append([])
+                    counter_name_list = 0
                 else:
-                    pass
+                    users_list[len(users_list) - 1].append(': ' + key + ' ' * (8 - len(key)))
+                    counter_name_list += 1
+            for element in range(len(users_list)):
+                if len(users_list[element]) != 0:
+                    print(str(users_list[element][:]))
+            #
+            input_all = input('=' * 84 + '\nTo witch user would you like to apply this command?\n'
+                                         'Press enter to apply to every one of the above:\n')
+            if input_all == '':
+                counter = 1
+                for key in users:
+                    print(str(counter) + ': User : ' + users[key].show_user()[0])
+                    print(' ' * 5 + 'Number of Computers: ' + str(len(users[key].show_user()[1])))
+                    print(' ' * 5 + 'Number of logs: ' + str(len(users[key].show_user()[2])))
+                    print(' ' * 8 + 'Computers')
+                    comp_count = 0
+                    comps_logs = users[key].comps_logs()
+                    for key in comps_logs:
+                        print(' ' * 10 + 'Computer: ' + key)
+                        sorted_logs = log_sorter(comps_logs[key])
+                        for key in sorted_logs:
+                            for element in sorted_logs[key]:
+                                file_org = element.show_org()
+                                print(' ' * 12 + 'File: ' + file_org + ' :' + element.show_log())
+                            print('-' * 84)
+                    comp_count += 1
+                    counter += 1
+            else:
+                for key in users:
+                    if input_all == key:
+                        user_ = input_all
+                        print('User : ' + users[user_].show_user()[0])
+                        print(' ' * 5 + 'Number of Computers: ' + str(len(users[user_].show_user()[1])))
+                        print(' ' * 5 + 'Number of logs: ' + str(len(users[user_].show_user()[2])))
+                        print(' ' * 8 + 'Computers')
+                        comps_logs = users[user_].comps_logs()
+                        for key in comps_logs:
+                            print(' ' * 10 + key)
+                            for element in comps_logs[key]:
+                                print(' ' * 12 + element.show_log())
+                    else:
+                        pass
+        elif input_user == 'c':
+            print('=' * 84 + '\n')
+            print("Enter: All user's data")
+            if input('What would you like to save?') == '':
+                # Creating user objects
+                users1 = user_creator(users, log_fracts)
+                #
+                text_lines = []
+                counter = 1
+                for key in users1:
+                    text_lines.append(str(counter) + ': User : ' + users1[key].show_user()[0])
+                    text_lines.append(' ' * 5 + 'Number of Computers: ' + str(len(users1[key].show_user()[1])))
+                    text_lines.append(' ' * 5 + 'Number of logs: ' + str(len(users1[key].show_user()[2])))
+                    text_lines.append(' ' * 8 + 'Computers')
+                    comp_count = 0
+                    comps_logs = users1[key].comps_logs()
+                    for key in comps_logs:
+                        text_lines.append(' ' * 10 + 'Computer: ' + key)
+                        sorted_logs = log_sorter(comps_logs[key])
+                        for key in sorted_logs:
+                            for element in sorted_logs[key]:
+                                file_org = element.show_org()
+                                text_lines.append(' ' * 12 + 'File: ' + file_org + ' :' + element.show_log())
+                            text_lines.append('-' * 84)
+                    comp_count += 1
+                    counter += 1
+                with open('All_User_INFO', 'w+') as opened_file:
+                    for element in range(len(text_lines)):
+                        opened_file.write(text_lines[element] + '\n')
 
 
 def log_sorter(array):
     Sorted_logs = {}
     for element in array:
-        if 'IN' in element.show_type():
+        if 'OUT' in element.show_type():
             Sorted_logs[element.show_license() + ' : ' + str(element.show_lic_nr())] = []
             Sorted_logs[element.show_license() + ' : ' + str(element.show_lic_nr())].append(element)
     for element in array:
-        if 'OUT' in element.show_type():
+        if 'IN' in element.show_type():
             for key in Sorted_logs:
                 if element.show_license() + ' : ' + str(element.show_lic_nr()) == key:
                     Sorted_logs[element.show_license() + ' : ' + str(element.show_lic_nr())].append(element)
@@ -157,12 +179,13 @@ def log_sorter(array):
 
 
 class log(object):
-    def __init__(self, time, type_, license_, username, computer_, how_many_lic=1):
+    def __init__(self, time, type_, license_, username, computer_, file_name, how_many_lic=1):
         self.time = time
         self.type_ = type_
         self.license_ = license_
         self.username = username
         self.computer = computer_
+        self.file_name = file_name
         self.how_many_licenses = how_many_lic
 
     def re_comp(self):
@@ -179,6 +202,9 @@ class log(object):
 
     def show_type(self):
         return self.type_
+
+    def show_org(self):
+        return self.file_name
 
 
 class user(object):
@@ -214,4 +240,4 @@ class user(object):
         return comps_dic
 
 
-user_logs()
+log_master()
